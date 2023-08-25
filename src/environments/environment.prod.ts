711219619c95d5f0b4a8ae9d892644e2385557b0
Nawin R <naveen.ravichandran@ysquaretechnology.com>
2023-08-25 19:10:57 +0530

    prod move

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://dev.api.amirthafashion.com',
+  apiURL: 'https://api.amirthafashion.com',
   imageUrl: "https://amirthafashions.s3.ap-south-1.amazonaws.com/"
 
 };
